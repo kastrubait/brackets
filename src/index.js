afd58ccf2c8378ae6add840483b1e09f4c65fd2c
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-22 23:15:57 +0300

    feat solution
    feat solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   let  chars = str.split('');
     let stack = [];
-    let open = ['{', '(', '['];
-    let close = ['}', ')', ']'];
+    let open = [];
+    let close = [];
     let closeIndex;
     let openIndex;
 
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] != bracketsConfig[i][1]) {
+            open[i] = bracketsConfig[i][0];
+            close [i] = bracketsConfig[i][1];
+        }
+    }
     for (var i = 0, len = chars.length; i < len; i++) {
         openIndex = open.indexOf(chars[i]);
         if (openIndex !== -1) {
